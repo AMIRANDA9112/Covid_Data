194f20e3bab383c8783b8138870fa6b44d5829bd
amiranda <Pipe91120707200>
2021-02-24 23:32:54 -0500

    str conversion

--- Backend/backendapi/api/daily.py
+++ Backend/backendapi/api/daily.py
@@ -11,24 +11,25 @@ def las_seven():
     date = datetime.datetime.strptime(sd, '%Y-%m-%d')
 
     for i in range(7):
-    date = date - datetime.timedelta(days=1)
+        date = date - datetime.timedelta(days=1)
+    
+        date = datetime.datetime.strftime(date, '%Y-%m-%d')
+    
+        print(date[:10])
+    
+        dater = date[:10]
+    
+        querystring = {"date": dater}
+    
+        headers = {
+            'x-rapidapi-key': "277ae669b3msh52f0bc0d898f114p159b11jsn27bf26ae14ea",
+            'x-rapidapi-host': "covid-19-data.p.rapidapi.com"
+        }
+    
+        response = requests.request("GET", url, headers=headers, params=querystring)
+    
+        print(response.text)
 
-    date = datetime.datetime.strftime(date, '%Y-%m-%d')
-
-    print(date[:10])
-
-    dater = date[:10]
-
-    querystring = {"date": dater}
-
-    headers = {
-        'x-rapidapi-key': "277ae669b3msh52f0bc0d898f114p159b11jsn27bf26ae14ea",
-        'x-rapidapi-host': "covid-19-data.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
-    print(response.text)
-
-
-    time.sleep(1)
+        date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    
+        time.sleep(1)
